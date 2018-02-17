622299ff9ece5e776703b12227963e5699a2f0f2
AFRIDI KAYAL <AFRIDI KAYAL@HP>
2018-02-17 18:47:36 +0530

    Add project files.

---
+++ VisualNQueen/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace VisualNQueen
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.canvas = new System.Windows.Forms.Panel();
+            this.Solve = new System.Windows.Forms.Button();
+            this.sizeSelect = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeSelect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel.Controls.Add(this.canvas);
+            this.panel.Controls.Add(this.Solve);
+            this.panel.Controls.Add(this.sizeSelect);
+            this.panel.Controls.Add(this.label1);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(506, 538);
+            this.panel.TabIndex = 0;
+            // 
+            // canvas
+            // 
+            this.canvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.canvas.BackColor = System.Drawing.SystemColors.Control;
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.canvas.Location = new System.Drawing.Point(12, 45);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(481, 480);
+            this.canvas.TabIndex = 4;
+            // 
+            // Solve
+            // 
+            this.Solve.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Solve.Location = new System.Drawing.Point(391, 10);
+            this.Solve.Name = "Solve";
+            this.Solve.Size = new System.Drawing.Size(102, 23);
+            this.Solve.TabIndex = 5;
+            this.Solve.Text = "Solve";
+            this.Solve.UseVisualStyleBackColor = true;
+            this.Solve.Click += new System.EventHandler(this.Solve_Click);
+            // 
+            // sizeSelect
+            // 
+            this.sizeSelect.Location = new System.Drawing.Point(167, 10);
+            this.sizeSelect.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.sizeSelect.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.sizeSelect.Name = "sizeSelect";
+            this.sizeSelect.Size = new System.Drawing.Size(42, 20);
+            this.sizeSelect.TabIndex = 1;
+            this.sizeSelect.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Board size / No. of queens (n)";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 538);
+            this.Controls.Add(this.panel);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(522, 577);
+            this.MinimumSize = new System.Drawing.Size(522, 577);
+            this.Name = "Form";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "N-Queens Solver";
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeSelect)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Solve;
+        private System.Windows.Forms.Panel canvas;
+        public System.Windows.Forms.NumericUpDown sizeSelect;
+    }
+}
+
---
+++ VisualNQueen/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace VisualNQueen
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Graphics g;
+        Brush black;
+        Brush white;
+
+        static int size;
+        
+        public Form()
+        {
+            InitializeComponent();
+            g = canvas.CreateGraphics();
+            black = Brushes.DarkSlateGray;
+            white = Brushes.FloralWhite;
+        }
+
+        private void Solve_Click(object sender, EventArgs e)
+        {
+            ResetBoard();
+            GetParams();
+
+            // Logic Here
+
+            int[,] board = new int[size, size];
+
+            // Initialize Board
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    board[i,j] = 0;
+                }
+            }
+
+            if (SolveNQ(board, 0))
+                DrawBoard(board);
+            else
+                MessageBox.Show("No Possible Solutions");
+        }
+
+        bool SolveNQ (int[,] board, int col)
+        {
+            // Solution Found
+            if (col >= size)
+                return true;
+
+            for(int i=0; i<size; i++)
+            {
+                if (isSafe(board, i, col))
+                {
+                    board[i, col] = 1;
+                    if (SolveNQ(board, col + 1))
+                        return true;
+                    board[i, col] = 0;
+                }
+            }
+
+            // Not Found
+            return false;
+        }
+
+        bool isSafe(int[,] b, int row, int col)
+        {
+            // Check Left
+            for (int i = 0; i < col; i++)
+            {
+                if (b[row, i] == 1)
+                    return false;
+            }
+
+            // Check Upper Left Diagonal
+            for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            {
+                if (b[i, j] == 1)
+                    return false;
+            }
+
+            // Check Lower Left Diagonal
+            for (int i = row, j = col; i < size && j >= 0; i++, j--)
+            {
+                if (b[i, j] == 1)
+                    return false;
+            }
+            return true;
+        }
+
+        void DrawBoard (int[,] board)
+        {
+            Bitmap queen = Properties.Resources.Queen;
+            int widthFactor = canvas.Width / size;
+            int heightFactor = canvas.Height / size;
+            bool b = true;
+            
+            for(int i=0; i<size; i++)
+            {
+                for(int j=0; j<size; j++)
+                {
+                    Size bound = new Size(widthFactor, heightFactor);
+                    Point point = new Point(widthFactor * j, heightFactor * i);
+                    if (b)
+                        g.FillRectangle(black, new Rectangle(point, bound));
+                    else
+                        g.FillRectangle(white, new Rectangle(point, bound));
+                    b = !b;
+                    if (board[i, j] == 1)
+                        g.DrawImage(queen, new Rectangle(point, bound));
+                }
+                if (size % 2 == 0)
+                    b = !b;
+            }
+        }
+
+        void ResetBoard ()
+        {
+            g.Clear(Color.White);
+        }
+
+        void GetParams()
+        {
+            size = (int)sizeSelect.Value;  
+        }
+    }
+}
